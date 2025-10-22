41c0b8251b1f7f1a14e12e26ae398b73f1e6016d
jordansmalls <138732777+jordansmalls@users.noreply.github.com>
2025-10-22 14:30:38 -0400

    Backend (#1)
    * feat: inital server setup
    
    * feat: implemented auth logic with rate limiting

---
+++ backend/index.js
@@ -0,0 +1,84 @@
+// server
+import express from "express"
+import connectDB from "./src/config/db.js"
+
+// middleware
+import morgan from "morgan"
+import cors from "cors"
+import dotenv from "dotenv"
+dotenv.config()
+import cookieParser from "cookie-parser"
+import { errorHandler, notFound } from "./src/middleware/error.middleware.js"
+
+// routes
+import auth from "./src/routes/auth.routes.js"
+
+connectDB();
+const app = express()
+const PORT = process.env.PORT || 4000
+
+
+
+
+
+
+// middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(cors())
+
+
+// logging
+if(process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"))
+} else {
+    app.use(morgan("tiny"))
+}
+
+
+// routes
+app.use("/api/auth", auth)
+
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "API is live",
+    timestamp: new Date().toISOString(),
+    environment: process.env.NODE_ENV || "development",
+  });
+});
+
+
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "OK",
+    timestamp: new Date().toISOString(),
+    uptime: process.uptime(),
+    memory: process.memoryUsage(),
+    version: process.version,
+  });
+});
+
+
+app.get("/test", (req, res) => {
+  return res.json({ status: 200, message: "API is live" });
+});
+
+// 404 handler
+app.use((req, res) => {
+  res.status(404).json({
+    error: "Route not found",
+    message: `Cannot ${req.method} ${req.originalUrl}`,
+  });
+});
+
+app.use(notFound);
+app.use(errorHandler);
+
+
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`))
+
+
+

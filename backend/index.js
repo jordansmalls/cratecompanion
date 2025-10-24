1c5b4a9a57ab6dbf1c81f5d999d97e5caf87421a
Jordan Smalls <smalls.jordan@gmail.com>
2025-10-24 13:56:33 -0400

    fix: update cors config

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,10 @@ const PORT = process.env.PORT || 4000
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
-app.use(cors())
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}))
 
 
 // logging
